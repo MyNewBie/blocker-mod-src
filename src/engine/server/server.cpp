0cf92f90f4cfce82aedab922a02aa3b8ece2ba25
Fluffy2000 <lessigbjoern@gmail.com>
2017-04-19 15:43:42 +0200

    Gay

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -506,7 +506,7 @@ const char *CServer::ClientClan(int ClientID)
 {
 	if(ClientID < 0 || ClientID >= MAX_CLIENTS || m_aClients[ClientID].m_State == CServer::CClient::STATE_EMPTY)
 		return "";
-	 +	if(m_aClients[ClientID].m_State == CServer::CClient::STATE_INGAME || m_aClients[ClientID].m_State == CServer::CClient::STATE_DUMMY)
+	if(m_aClients[ClientID].m_State == CServer::CClient::STATE_INGAME || m_aClients[ClientID].m_State == CServer::CClient::STATE_DUMMY)
 		return m_aClients[ClientID].m_aClan;
 	else
 		return "";
