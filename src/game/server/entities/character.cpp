0e883044e07f2f1f9b5e62c806dd93b5bd188e3f
Eicosaan <marvin.gans@gmail.com>
2017-06-09 21:10:15 +0200

    gg

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -69,7 +69,7 @@ bool CCharacter::Spawn(CPlayer *pPlayer, vec2 Pos)
 	
 	m_LovelyLifeSpan = Server()->TickSpeed(); // hearty
 	
-	if (pPlayer->m_IsBallActivated)
+	if (pPlayer->m_IsBallSpawned)  // done ,-, <,<
 		pPlayer->m_pBall = new CBall(&GameServer()->m_World, m_Pos, pPlayer->GetCID());
 
 
