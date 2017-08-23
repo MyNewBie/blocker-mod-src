d02660c0b1647855a0aac917688694aaa5fa0653
Loic <loic.ka38@gmail.com>
2017-08-23 22:11:40 +0200

    fix passive for temp players

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1948,10 +1948,13 @@ void CCharacter::HandleTiles(int Index)
 	// passive
 	if (g_Config.m_SvWbProt != 0)
 	{
-		if (g_Config.m_SvWbProt == 1 || (g_Config.m_SvWbProt == 2 && ((m_pPlayer->m_AccData.m_Vip || m_pPlayer->Temporary.m_PassiveMode))))
+		if (g_Config.m_SvWbProt == 1 || (g_Config.m_SvWbProt == 2 && (m_pPlayer->m_AccData.m_Vip || m_pPlayer->Temporary.m_PassiveMode)))
 		{
-			if ((m_TileIndex == TILE_PASSIVE_IN || m_TileFIndex == TILE_PASSIVE_IN) && !m_PassiveMode && m_pPlayer->m_Passive)
+			if ((m_TileIndex == TILE_PASSIVE_IN || m_TileFIndex == TILE_PASSIVE_IN) && !m_PassiveMode)
 			{
+				if(m_pPlayer->m_AccData.m_Vip && !m_pPlayer->m_Passive)
+					return;
+
 				GameServer()->SendChatTarget(GetPlayer()->GetCID(), "Passive mode enabled!");
 				m_ThreeSecondRule = false;
 				m_PassiveMode = true;
